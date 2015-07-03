2d10270634134d59cbad6363962be1f41e0b1d92
Kit Fung <sa9510@gmail.com>
2015-07-03 16:48:22 +0800

    1.added google analysis code and the event tracker 2. modified the attribute in the enquire form to fit the pandaform

---
+++ js/enquiry.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	if($('#enquiry-form').show()){
+		$('.fakeUpload').on('click',function (){
+			$('#file').click();
+		});
+		$('#file').on('change',function(){
+			var filename = this.value;
+	    var lastIndex = filename.lastIndexOf("\\");
+	    if (lastIndex >= 0) {
+	        filename = filename.substring(lastIndex + 1);
+	    }
+	    $('#filename').html(filename);
+		});
+		 $('.submitForm').on('click', function(){
+		 	var checkedAtLeastOne = false;
+			$('input[type="checkbox"]').each(function() {
+			    if ($(this).is(":checked")) {
+			        checkedAtLeastOne = true;
+			    }
+			});
+			if(!checkedAtLeastOne)
+				$('#checkbox6').prop("checked",true);
+			$('#submit').click();
+			ga('send', 'event', {
+			    eventCategory: 'Enquiry',
+			    eventAction: 'Submit enquiry form'
+			});
+		});
+	}
+});
