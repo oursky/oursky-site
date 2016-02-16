d231f57c6a4ae37633d625876f9d1c98fd1daa51
Rick Mak <rick.mak@gmail.com>
2016-02-16 17:59:56 +0800

    Use jquery validation according to documentation
    http://jqueryvalidation.org/validate

--- js/enquiry.js
+++ js/enquiry.js
@@ -1,42 +1,42 @@
+
 $(document).ready(function() {
-  if ($('#enquiry-form').show()) {
-    $('.fakeUpload').on('click', function() {
-      $('#file').click();
-    });
-    $('#file').on('change', function() {
-      var filename = this.value;
-      var lastIndex = filename.lastIndexOf("\\");
-      if (lastIndex >= 0)
-        filename = filename.substring(lastIndex + 1);
-      $('#filename').html(filename);
-    });
-    $('.submitForm').on('click', function() {
-      $('#enquiry-form').validate({
-        rules: {
-          services: {
-            required: true,
-            minlength: 1
-          }
-        },
-        messages: {
-          services: "Please select at least one service"
-        },
-        errorPlacement: function(error, element) {
-          var elements = $("[name ='" + $(element).attr("name") + "']");
-          if (elements.length > 1) {
-            error.insertAfter($(elements[elements.length-1]).next());
-          } else {
-            error.insertAfter(element);
-          }
-        }
-      });
+  $('.fakeUpload').on('click', function() {
+    $('#file').click();
+  });
+  $('#file').on('change', function() {
+    var filename = this.value;
+    var lastIndex = filename.lastIndexOf("\\");
+    if (lastIndex >= 0)
+      filename = filename.substring(lastIndex + 1);
+    $('#filename').html(filename);
+  });
+  $('#enquiry-form').validate({
+    rules: {
+      services: {
+        required: true,
+        minlength: 1
+      }
+    },
+    success: function(label) {
+      label.addClass("valid").text("Ok!");
+    },
+    errorPlacement: function(error, element) {
+      var elements = $("[name ='" + $(element).attr("name") + "']");
+      if (elements.length > 1) {
+        error.insertAfter($(elements[elements.length-1]).next());
+      } else {
+        error.insertAfter(element);
+      }
+    },
+    submitHandler: function(form) {
       ga('send', 'event', {
         eventCategory: 'Enquiry',
         eventAction: 'Submit enquiry form'
       });
-      //window['optimizely'] = window['optimizely'] || [];
-      //window['optimizely'].push(["trackEvent", "enquiryformSubmit"]);
-      $('#submit').click();
-    });
-  }
+      window['optimizely'] = window['optimizely'] || [];
+      window['optimizely'].push(["trackEvent", "enquiryformSubmit"]);
+      form.submit();
+    }
+  });
 });
+
